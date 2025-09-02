b4a4ada782bf1228519b5c593393f105d595cc46
Bernardo <beftorres@hotmail.com>
2025-09-02 17:33:34 +0200

    relaxing dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='stable-audio-tools',
     version='0.0.19',
-    url='https://github.com/Stability-AI/stable-audio-tools.git',
     author='Stability AI',
     description='Training and inference tools for generative audio models from Stability AI',
     packages=find_packages(),  
